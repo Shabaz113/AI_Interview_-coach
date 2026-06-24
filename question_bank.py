684730c43f764b6bef9bba7c7741202b6d9bb86b
Mohammed Shabaz Khan <shabazkhan.broken1@gmail.com>
2026-06-24 12:48:13 +0530

    Create question_bank.py

---
+++ question_bank.py
@@ -0,0 +1,21 @@
+questions = {
+    "Data Scientist": [
+        "Tell me about yourself.",
+        "Explain a machine learning project you worked on.",
+        "What is overfitting?",
+        "How do you handle missing values?"
+    ],
+
+    "Software Engineer": [
+        "Tell me about yourself.",
+        "What is OOP?",
+        "Explain time complexity.",
+        "Describe a challenging project."
+    ],
+
+    "General": [
+        "Why should we hire you?",
+        "Tell me about a failure.",
+        "Where do you see yourself in 5 years?"
+    ]
+}
